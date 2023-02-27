f94848bc60705c8409d1f06a14f3b9df468aeed3
missa999 <missaouim88@gmail.com>
2023-02-27 09:25:16 +0100

    update

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 mongoose.set("strictQuery", false); 
 
 // Connexion à la base données
-mongoose.connect(process.env.DATABASECLOUD,{ 
+mongoose.connect(process.env.DATABASE,{ 
 useNewUrlParser: true, 
 useUnifiedTopology: true
  }) 
